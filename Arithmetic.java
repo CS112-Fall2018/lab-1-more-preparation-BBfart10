381b624e34182e0b3b44c4393327b4506b9f0ca8
Laura Villagomez <BBiWiGGUMS@Lauras-MacBook-Air-2.local>
2018-08-29 13:26:25 -0700

    Arithmetic code

---
+++ Arithmetic.java
@@ -0,0 +1,30 @@
+
+
+public class Arithmetic {
+
+	public static void main(String[] args) {  
+
+		int a, b;
+		a = 1;
+		b = 2;
+		float x, y;
+		x = 3.5f;
+		y = 4.3f;
+
+		System.out.println("a + b = " +a+b);
+		System.out.println("a + x = " +a+x);
+		System.out.println("a + y = " +a+y);
+		System.out.println("b + x = " +b+x);
+		System.out.println("b + y = " +b+y);
+
+		System.out.println("a / b = " +a/b);
+		System.out.println("a / x = " +a/x);
+		System.out.println("a / y = " +a/y);
+		System.out.println("b / x = " +b/x);
+		System.out.println("b / y = " +b/y);
+
+
+
+	}
+
+}
